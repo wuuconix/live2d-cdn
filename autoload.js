afc637f567ef19fdfafa4fbee75328caa73ad34a
wuuconix <1521900139@qq.com>
2022-04-05 22:18:59 +0800

    修改cdn url为@main

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/wuuconix/live2d-cdn@latest/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/wuuconix/live2d-cdn@main/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			cdnPath: "https://cdn.jsdelivr.net/gh/wuuconix/live2d-cdn@latest/"
+			cdnPath: live2d_path
 		});
 	});
 }
