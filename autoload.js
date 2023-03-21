cfb01f51054c6d09c71a6498f50a931ef5653b47
wuuconix <wuuconix@qq.com>
2023-03-21 14:49:40 +0800

    使用 https://github.com/stevenjoezhang/live2d-widget 新项目结构

--- autoload.js
+++ autoload.js
@@ -1,7 +1,7 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/wuuconix/live2d-cdn@main/";
+const live2d_path = "/";
 // 模型texture图片地址
-const modelTextures = "https://cdn.jsdelivr.net/gh/wuuconix/live2d-cdn@main/model/xiaomai/umaru2048/texture_00.png";
+const modelTextures = "/model/xiaomai/umaru2048/texture_00.png";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -16,34 +16,33 @@ function loadExternalResource(url, type) {
 			tag = document.createElement("script");
 			tag.src = url;
 		} else if (type === "png") {
-      tag = new Image()
-      tag.src = url;
-    }
+			tag = new Image()
+			tag.src = url;
+		}
 		if (tag) {
 			tag.onload = () => resolve(url);
 			tag.onerror = () => reject(url);
-      if (["js", "css"].includes(type)) {
-        document.head.appendChild(tag);
-      }
+			if (["js", "css"].includes(type)) {
+				document.head.appendChild(tag);
+			}
 		}
 	});
 }
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
-  loadExternalResource(modelTextures, "png").then(() => {         // 首先确保模型图片已经加载完成
-    Promise.all([
-      loadExternalResource(live2d_path + "waifu.css", "css"),
-      loadExternalResource(live2d_path + "live2d.min.js", "js"),
-      loadExternalResource(live2d_path + "waifu-tips.js", "js")
-    ]).then(() => {
-      initWidget({
-        waifuPath: live2d_path + "waifu-tips.json",
-        cdnPath: live2d_path
-      });
-    });
-  });
+	loadExternalResource(modelTextures, "png").then(() => {         // 首先确保模型图片已经加载完成
+		Promise.all([
+			loadExternalResource(live2d_path + "waifu.css", "css"),
+			loadExternalResource(live2d_path + "live2d.min.js", "js"),
+			loadExternalResource(live2d_path + "waifu-tips.js", "js")
+		]).then(() => {
+			// 配置选项的具体用法见 https://github.com/stevenjoezhang/live2d-widget/blob/master/README.md
+			initWidget({
+				waifuPath: live2d_path + "waifu-tips.json",
+				cdnPath: live2d_path,
+				tools: ["hitokoto", "asteroids", "photo", "info", "quit"]
+			});
+		});
+	});
 }
-// initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
-// API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
-// 初始化看板娘会自动加载指定目录下的 waifu-tips.json
